b36050da5090f468f04f3241802f1a3126f0ed0f
jin-vita <jhj9144@gmail.com>
2023-07-03 16:04:16 +0900

    feat list view

--- app/src/main/java/com/jinvita/testlistview/MainActivity.kt
+++ app/src/main/java/com/jinvita/testlistview/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.jinvita.testlistview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.jinvita.testlistview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val dummy1 = listOf(
+        "철이랑 수철이랑 수",
+        "영이랑 희영이랑 희",
+        "연이랑 수연이랑 수",
+        "강이랑 혁강이랑 혁",
+        "용이랑 찬용이랑 찬",
+        "철이랑 수철이랑 수",
+        "영이랑 희영이랑 희",
+        "연이랑 수연이랑 수",
+        "강이랑 혁강이랑 혁",
+        "용이랑 찬용이랑 찬",
+        "철이랑 수철이랑 수",
+        "영이랑 희영이랑 희",
+        "연이랑 수연이랑 수",
+        "강이랑 혁강이랑 혁",
+        "용이랑 찬용이랑 찬",
+        "철이랑 수철이랑 수",
+        "영이랑 희영이랑 희",
+        "연이랑 수연이랑 수",
+        "강이랑 혁강이랑 혁",
+        "용이랑 찬용이랑 찬",
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dummy1)
+        with(binding) {
+            listView.adapter = adapter
+            listView.setOnItemClickListener { parent, view, position, id ->
+                textTitle.text = parent.getItemAtPosition(position).toString()
+            }
+        }
     }
 }
